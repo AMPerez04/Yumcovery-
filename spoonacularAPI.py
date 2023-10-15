8171bfbdc88e8d3506f67f48e25cfcbb2230a646
David Tejuosho <tejuoshodavid@gmail.com>
2023-10-15 15:42:20 -0500

    refactored spoonacular.py

--- spoonacularAPI.py
+++ spoonacularAPI.py
@@ -1,73 +1,61 @@
-import requests
 import random
-import json
-import sys
 
-api_key = '9352cf695fe2426f89998c232f7fc973'
+import requests
 
-def get_menu_items(query, calorie_limit):
-    # Replace 'your_endpoint' with the actual endpoint from the Spoonacular API
-    url = 'https://api.spoonacular.com/food/menuItems/search'
-    url_with_params = f'{url}?query={query}&apiKey={api_key}&maxCalories={calorie_limit}'
+api_key = '9352cf695fe2426f89998c232f7fc973'  # privacy doesn't matter, this is a free api
 
 
-    response = requests.get(url_with_params)
+def get_menu_items(min_protein=None, max_protein=None, min_fat=None, max_fat=None, min_carbs=None, max_carbs=None,
+                   min_calories=None, max_calories=None):
+    if ((min_protein is None and max_protein is None)
+            or (min_fat is None and max_fat is None)
+            or (min_carbs is None and max_carbs is None)
+            or (min_calories is None and max_calories is None)):
+        raise Exception("You must specify at least one min/max pair of macros")
 
-    if response.status_code == 200:
-        menu_items = response.json()
-        return menu_items
-    else:
-        print(f'Failed to retrieve menu items: {response.status_code}')
-        return None
+    if min_protein is None or min_fat is None or min_carbs is None or min_calories is None:
+        min_protein = max_protein * 0.7
+        min_fat = max_fat * 0.7
+        min_carbs = max_carbs * 0.7
+        min_calories = max_calories * 0.7
 
-def get_recipe(minProtein = 0, maxProtein = sys.maxsize, minFat = 0, maxFat = sys.maxsize, minCarbs = 0, maxCarbs = sys.maxsize, minCalories = 0, maxCalories = sys.maxsize):
-    url = 'https://api.spoonacular.com/recipes/complexSearch'
+    if max_protein is None or max_fat is None or max_carbs is None or max_calories is None:
+        max_protein = min_protein * 1.4
+        max_fat = min_fat * 1.4
+        max_carbs = min_carbs * 1.4
+        max_calories = min_calories * 1.4
+
+    num_meals = 1
+    endpoint = 'https://api.spoonacular.com/recipes/complexSearch'
     offset = int(random.random() * 100)
-    url_with_params = f'{url}?apiKey={api_key}&minCalories={minCalories}&maxProtein={maxProtein}&minProtein={minProtein}&maxCarbs={maxCarbs}&minCarbs={minCarbs}&maxFat={maxFat}&minFat={minFat}&maxCalories={maxCalories}&intstructionsRequired=TRUE&offset={offset}&number=1'
+    url = f'''
+    {endpoint}?apiKey={api_key}&minCalories={min_calories}&maxProtein={max_protein}&minProtein={min_protein}
+    &maxCarbs={max_carbs}&minCarbs={min_carbs}&maxFat={max_fat}&minFat={min_fat}&maxCalories={max_calories}
+    &intstructionsRequired=TRUE&offset={1}&number={num_meals}'''
 
-    name = requests.get(url_with_params)
-    if name.status_code == 200:
-        recipe = name.json()
+    response = requests.get(url)
+    if response.status_code == 200:
+        meals = response.json()
     else:
-        # print(f'Failed to retrieve menu items: {response.status_code}')
-        return None
-    if recipe:
-        id = recipe["results"][0]["id"]
-    else:
-        return None
-    url = f'https://api.spoonacular.com/recipes/{id}/ingredientWidget.json?apiKey={api_key}'
-    url2 = f'https://api.spoonacular.com/recipes/{id}/analyzedInstructions?apiKey={api_key}'
-    url3 = f'https://api.spoonacular.com/food/menuItems/{id}/nutritionLabel.png?apiKey={api_key}'
-    ingredients = requests.get(url)
-    instructions = requests.get(url2)
-    nutLabel = requests.get(url3)
-    if (ingredients.status_code == 200) and (instructions.status_code == 200):
-        recipe_ingredients = ingredients.json()
-        recipe_instructions = instructions.json()
+        raise Exception(f'Failed to retrieve meal: {response.status_code}')
 
-        # if recipe_ingredients and recipe_instructions:
-        #     print()
-        #     print(f'{recipe["results"][0]["title"]}')
-        #     #print(nutLabel)
-        #     print()
-        #     print("ingredients:")
-        #     for item in recipe_ingredients["ingredients"]:
-        #         print(f'{item["name"]}, {item["amount"]["us"]["value"]} {item["amount"]["us"]["unit"]}')
+    meals_list = []
+    for i, meal in enumerate(meals["results"]):
+        recipe_id = meal["id"]
 
-        #     print()
-        #     print("instructions:")
-        #     for item in recipe_instructions[0]["steps"]:
-        #         print()
-        #         print(f'{item["number"]}. {item["step"]}')
+        ingredient_url = f'https://api.spoonacular.com/recipes/{recipe_id}/ingredientWidget.json?apiKey={api_key}'
+        instruction_url = f'https://api.spoonacular.com/recipes/{recipe_id}/analyzedInstructions?apiKey={api_key}'
 
-    response_dict = {
-        'name': name,
-        'ingredients': ingredients,
-        'instructions': instructions
-    }
+        ingredients = requests.get(ingredient_url)
+        instructions = requests.get(instruction_url)
+        if (ingredients.status_code == 200) and (instructions.status_code == 200):
+            recipe_ingredients = ingredients.json()["ingredients"]
+            recipe_instructions = instructions.json()
 
-    return response_dict
-        
-# Usage:
-# menu_items = get_recipe(minProtein = 0, maxProtein = 20, minFat = 0, maxFat = 20, minCarbs = 0, maxCarbs = 20, minCalories = 0, maxCalories = 700)
+        meals_list.append({
+            'name': meals['results'][0]['title'],
+            'ingredients': recipe_ingredients,
+            'instructions': recipe_instructions
+        })
 
+    return meals_list
