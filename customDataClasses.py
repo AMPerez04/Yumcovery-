f0fcbc35c57ab1c1ccb111c24ec1d1ed75c65253
David Cui <d.cui@wustl.edu>
2023-10-14 20:31:33 -0500

    fixed conflict in data

--- customDataClasses.py
+++ customDataClasses.py
@@ -3,7 +3,6 @@ from enum import Enum
 from datetime import date
 from pprint import pprint
 
-<<<<<<< Updated upstream
 cumulative_calorie_expenditure_over_time = [0.038,
                                             0.037,
                                             0.038,
@@ -29,37 +28,6 @@ cumulative_calorie_expenditure_over_time = [0.038,
                                             0.037,
                                             0.038,
                                             0.038]
-=======
-
-
-cumulative_calorie_expenditure_over_time = [
-    0.038,
-    0.075,
-    0.113,
-    0.150,
-    0.188,
-    0.225,
-    0.263,
-    0.301,
-    0.343,
-    0.385,
-    0.427,
-    0.469,
-    0.511,
-    0.553,
-    0.595,
-    0.637,
-    0.679,
-    0.721,
-    0.763,
-    0.805,
-    0.847,
-    0.887,
-    0.924,
-    0.962,
-    1.000
-]
->>>>>>> Stashed changes
 
 
 class Goal(Enum):
@@ -73,7 +41,7 @@ class Sex(Enum):
     female = 2
 
 
-class PA(Enum): #Physical Activity
+class PA(Enum):  # Physical Activity
     Sedentary = 1
     Lightly_active = 2
     Moderately_active = 3
